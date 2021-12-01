3ee90877e2786b50e8d26a8b06fa42952d2aeb38
walteremoore <walterenriquemoore@gmail.com>
2021-12-01 17:58:25 -0500

    cambio loco

--- Unidad1-3.py
+++ Unidad1-3.py
@@ -444,4 +444,4 @@
 #    elif actual>12000:
 #        print("Su salario no tiene un aumento programado, quedando en la suma de $", actual)
 #Un comercio ofrece un descuento del 15% sobre el total de la compra si esta supera los $1000. Obtenga para determinado cliente cuánto deberá pagar finalmente por su compra y el descuento obtenido, si es que corresponde.
-# prueba
+# prueba 2
